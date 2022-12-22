f97e9d619d78cbaceeb9d55531c21b38ab2775ad
indigoboy19 <indigoBoy19@protonmail.com>
2022-12-22 11:15:08 -0300

    ElfCamp.calculateElvesTotalCalories

--- code/src/main/scala/2022/ElfCamp.scala
+++ code/src/main/scala/2022/ElfCamp.scala
@@ -1,3 +1,14 @@
 package `2022`
 
-case class ElfCamp()
+import  `2022`.domain.model.ElfFood
+import  `2022`.domain.model.ElfFoodWithTotalCalories
+
+case class ElfCamp():
+    def calculateElvesTotalCalories(elvesFood: List[ElfFood]): List[ElfFoodWithTotalCalories] =
+        elvesFood match
+            case List() => List.empty[ElfFoodWithTotalCalories]
+            case head :: tail =>
+                val sameElfFood = tail.filter(_.doesItBelongToElf(head))
+                val differentElfFood = tail.filterNot(_.doesItBelongToElf(head))
+                val totalCalories = sameElfFood.foldLeft(head)(_.sumCalories(_))
+                ElfFoodWithTotalCalories(head.elfId, sameElfFood, totalCalories) :: calculateElvesTotalCalories(differentElfFood)
