8903890f6bf51610ea403c19adf980f2fbc62ad0
indigoboy19 <indigoBoy19@protonmail.com>
2022-12-24 16:15:25 -0300

    FirstExercise Test

---
+++ code/src/test/scala/2022/1/ResolutionTest.scala
@@ -0,0 +1,24 @@
+import `2022`.ElfCamp
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import `2022`.domain.repository.elfFood.ElfFoodRepository
+import `2022`.domain.repository.elfFood.ElfFoodRepositoryFromFile
+import `2022`.domain.controller.elfFood.ElfFoodController
+
+import org.scalatest.OptionValues.convertOptionToValuable
+
+class ResolutionTest extends AnyFlatSpec {
+
+    "Elf in camp, who is very lonely" should "has 6000 calories of food" in {
+        assert(elfFoodRepository("src/test/scala/2022/1/resources/oneElfFood").value == 6000.0)
+    }
+
+    def elfFoodRepository(path: String) =
+        ElfFoodRepositoryFromFile(path)
+            .map(elfFoodRepository =>
+                ElfFoodController(ElfCamp(), elfFoodRepository).getElfMaxCalories.calories
+            )
+
+    
+}
